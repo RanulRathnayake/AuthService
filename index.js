9663086e874bcf37c0aadc7155ee262e9faf825a
RanulRathnayake <rathnayakerg.20@uom.lk>
2025-03-17 05:17:17 +0530

    Setup the project/DB config/

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+app.use(cors);
+app.use(helmet);
+app.use(express.json);
+app.use(cookieParser);
+app.use(express.urlencoded({extended:true}));
+
+mongoose.connect(process.env.MONGO_URI).then(()=>{
+    console.log("Database is connected");
+}).catch((err)=>{
+    console.log(err);
+})
+
+app.get('/',(req,res)=>{
+    res.json({message:"HEllo from the sever"});
+})
+
+app.listen(PORT = process.env.PORT, ()=>{
+    console.log("server is listening to port " + PORT);
+})
---
+++ models/userModel.js
@@ -0,0 +1,35 @@
+const { verify } = require('jsonwebtoken');
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    email:{
+        type: String,
+        required: [true,'Email is required!'],
+        trim: true,
+        unique: [true, 'Email must be unique!'],
+        minilength: [5, 'Email must have 5 characters!'],
+        lowercase: true
+    },
+    password:{
+        type: String,
+        required: [true,'Password is required'],
+        trim: true,
+        Select: false,
+    },
+    verified:{
+        true: Boolean,
+        default: false
+    },
+    verificationCodeValidation:{
+        type: Number,
+        Select: false
+    },
+    forgotPasswordCode:{
+        type: String,
+        Select: false
+    },
+    forgotPasswordCodeValidation:{
+        type: Number,
+        Select: false
+    }
+})
